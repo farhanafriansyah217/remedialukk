2e8401e3859fdc0a61dfd529732c3e4d25416c10
melatikusumapertiwi <pmelatikusuma@gmail.com>
2021-06-01 13:37:42 -0600

    mengecek login admin dan petugas

---
+++ cek_login2.php
@@ -0,0 +1,59 @@
+<?php 
+include 'koneksi.php';
+$user=$_POST['username'];
+$pass=$_POST['password'];
+$sql=$mysqli -> query("select * from petugas where username='$user' and password='$pass' ");
+$cek=mysqli_num_rows($sql);
+
+	if ($cek>0) //jika ketemu
+ 	{
+		$data=mysqli_fetch_array($sql);
+		if ($data['level']=="admin") 
+		{
+		session_start();
+		$_SESSION['id_petugas']=$data['id_petugas'];
+		$_SESSION['user']=$user;
+		$_SESSION['nama']=$data['nama_petugas'];
+		$_SESSION['level']=$data['level'];
+
+		header('location:admin/admin.php');
+		}
+		else if ($data['level']=="petugas")
+		{
+		session_start();
+		$_SESSION['user']=$user;
+		$_SESSION['nama']=$data['nama_petugas'];
+		$_SESSION['level']=$data['level'];
+
+		header('location:petugas/petugas.php');
+		}
+
+	}
+	else
+	{
+		?>
+		<script type="text/javascript">
+			alert ('Username Atau Password tidak ditemukan');
+			window.location="index2.php";
+		</script>
+<?php
+	}
+
+/*$result = $mysqli -> query("SELECT * FROM masyarakat where username ='$user' AND password='$pass'");
+if ($result) {
+	$data=mysqli_fetch_array($result);
+		session_start();
+		$_SESSION['nama']=$user;
+		$_SESSION['nik']=$data['nik'];
+		header('location:masyarakat.php');
+	}
+	else
+	{
+		?>
+		<script type="text/javascript">
+			alert ('Username Atau Password tidak ditemukan');
+			window.location="index.php";
+		</script>
+<?php
+}*/
+?>
